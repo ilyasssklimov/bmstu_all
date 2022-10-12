9a8ad55a136cf9d9946f0f5a2b0cf59add454256
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-10-13 01:26:30 +0300

    [ALL] delete extra files && add modeling lections

--- DataSecurity/labs/lab_01/install.bat
+++
@@ -1,6 +0,0 @@
-pip install pyinstaller
-pyinstaller --onefile --clean .\installer.py
-copy .\dist\installer.exe .\installer.exe
-del .\installer.spec
-rd /s /q .\dist
-rd /s /q .\build
--- DataSecurity/labs/lab_02/config.py
+++
@@ -1,3 +0,0 @@
-CHAR_DIR = r'data\char'
-BYTE_DIR = r'data\byte'
-FILE_DIR = r'data\files'
