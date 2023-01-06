95f725cb4e7e4383c3367557cd11186a3ba101bd
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2023-01-06 19:44:42 +0300

    [T] done client service integration tests

--- sem_07/WEB/labs/back/lib/business_logic/tests/src/int_test_service/client.cpp
+++ sem_07/WEB/labs/back/lib/business_logic/tests/src/int_test_service/client.cpp
@@ -12,7 +12,7 @@
 #include "repository/comment.h"
 
 
-TEST(SERVICE_CLIENT_TEST, ADD_COMMENT)
+TEST(SERVICE_CLIENT_INT_TEST, ADD_COMMENT)
 {
     // Arrange
     UserBL user = UserBuilder().build();
@@ -58,49 +58,71 @@ TEST(SERVICE_CLIENT_TEST, ADD_COMMENT)
 }
 
 
-// TEST(SERVICE_CLIENT_TEST, GET_POSTS_BY_FILTERS)
-// {
-//     // Arrange
-//     auto post_builder = PostBuilder();
-//     PostBL post_1 = post_builder.with_date("01.01.1970").build(false);
-//     PostBL post_2 = post_builder.with_date("02.01.1970").build(false);
+TEST(SERVICE_CLIENT_INT_TEST, GET_POSTS_BY_FILTERS)
+{
+    // Arrange
+    UserBL user = UserBuilder().build();
 
-//     auto user_repo = new TestUserRepository(std::vector<UserBL>());
-//     auto post_repo = new TestPostRepository({ post_1, post_2 }); 
-//     auto comment_repo = new TestCommentRepository(std::vector<CommentBL>());  
-//     auto service = ClientService(
-//         std::shared_ptr<TestPostRepository>(post_repo),
-//         std::shared_ptr<TestCommentRepository>(comment_repo),
-//         std::shared_ptr<TestUserRepository>(user_repo)
-//     );
+    auto db = std::make_shared<PGDatabaseAsync>(PGDatabaseParams);
+    add_user_to_db(db, user);
+    int user_id = db->get_user_id(user.get_login());
 
-//     // Act
-//     std::vector<PostBL> get_posts = service.get_posts(post_1.get_date(), "", "", "");
+    auto builder = PostBuilder();
+    PostBL post_1 = builder
+                    .with_date("01.01.1970")
+                    .with_author_id(user_id)
+                    .build(false);
+    PostBL post_2 = builder
+                    .with_date("02.01.1970")
+                    .with_author_id(user_id)
+                    .build(false);
 
-//     // Assert
-//     EXPECT_EQ(get_posts[0], post_1);
-//     EXPECT_EQ(get_posts.size(), 1);
-// }
+    add_post_to_db(db, post_1);
+    add_post_to_db(db, post_2);
+
+    int id_1 = db->get_post_id(Post(post_1));
+    int id_2 = db->get_post_id(Post(post_2));
+    
+    do_visible_post(db, id_1, post_1);
+    do_visible_post(db, id_2, post_2);
+
+    auto user_repo = std::make_shared<UserRepository>(db);
+    auto post_repo = std::make_shared<PostRepository>(db);
+    auto comment_repo = std::make_shared<CommentRepository>(db);
+    auto service = ClientService(post_repo, comment_repo, user_repo);
+
+    // Act
+    std::vector<PostBL> posts = service.get_posts(post_1.get_date(), "", "", "");
+
+    // Assert
+    EXPECT_EQ(posts[0], post_1);
+    EXPECT_EQ(posts.size(), 1);
+
+    db->delete_posts();
+    db->delete_users();
+}
 
 
-// TEST(SERVICE_CLIENT_TEST, UPDATE_USER)
-// {
-//     // Arrange
-//     UserBL user = UserBuilder().build();
+TEST(SERVICE_CLIENT_INT_TEST, UPDATE_USER)
+{
+    // Arrange
+    UserBL user = UserBuilder().build();
 
-//     auto user_repo = new TestUserRepository({ user });
-//     auto post_repo = new TestPostRepository(std::vector<PostBL>()); 
-//     auto comment_repo = new TestCommentRepository(std::vector<CommentBL>());  
-//     auto service = ClientService(
-//         std::shared_ptr<TestPostRepository>(post_repo),
-//         std::shared_ptr<TestCommentRepository>(comment_repo),
-//         std::shared_ptr<TestUserRepository>(user_repo)
-//     );
+    auto db = std::make_shared<PGDatabaseAsync>(PGDatabaseParams);
+    add_user_to_db(db, user);
+    int user_id = db->get_user_id(user.get_login());
 
-//     // Act
-//     UserBL upd_user = service.update(0, "new_name", "", "", "", "");
+    auto user_repo = std::make_shared<UserRepository>(db);
+    auto post_repo = std::make_shared<PostRepository>(db);
+    auto comment_repo = std::make_shared<CommentRepository>(db);
+    auto service = ClientService(post_repo, comment_repo, user_repo);
 
-//     // Assert
-//     EXPECT_EQ(upd_user, user_repo->get_user(0));
-//     EXPECT_EQ(user_repo->get_user(0).get_name(), "new_name");
-// }
+    // Act
+    UserBL upd_user = service.update(user_id, "new_name", "", "", "", "");
+
+    // Assert
+    EXPECT_EQ(upd_user, user_repo->get_user(user_id));
+    EXPECT_EQ(user_repo->get_user(user_id).get_name(), "new_name");
+
+    db->delete_users();
+}
